bcfd524dec90933f7ff9b6de586fdf968620e2fa
barbar17 <akbarmaulana644@gmail.com>
2025-12-26 21:39:18 +0700

    create db + fix form barang + add barang interface

--- app/components/InventoryForm.tsx
+++ app/components/InventoryForm.tsx
@@ -1,17 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import { Form, Button, Row, Col } from 'react-bootstrap';
 
+interface Barang {
+  id: string,
+  nama: string,
+  jenis: "Goods" | "IT Device",
+  qty: number,
+  tahunPengadaan: string,
+  kondisi: "Baru" | "Bagus" | "Kurang Baik" | "Rusak",
+  lokasi: string,
+  statusOperasional: boolean,
+  ket: string,
+  ip: string,
+  mac: string
+}
+
+const barangDefaultValue: Barang = {
+  id: "",
+  nama: "",
+  jenis: "Goods",
+  qty: 1,
+  tahunPengadaan: "",
+  kondisi: "Baru",
+  lokasi: "",
+  statusOperasional: true,
+  ket: "",
+  ip: "",
+  mac: "",
+}
+
 const InventoryForm = ({ onSave, editingItem }: { onSave: any, editingItem: any }) => {
-  const [item, setItem] = useState({
-    id: '',
-    name: '',
-    type: 'goods', // 'goods' or 'it_device'
-    quantity: 1,
-    location: '',
-    description: '',
-    ipAddress: '', // For IT devices
-    macAddress: '', // For IT devices
-  });
+  const [item, setItem] = useState<Barang>(barangDefaultValue);
 
   useEffect(() => {
     if (editingItem) {
@@ -30,16 +49,7 @@ const InventoryForm = ({ onSave, editingItem }: { onSave: any, editingItem: any 
       item.id = Date.now().toString(); // Simple ID generation
     }
     onSave(item);
-    setItem({
-      id: '',
-      name: '',
-      type: 'goods',
-      quantity: 1,
-      location: '',
-      description: '',
-      ipAddress: '',
-      macAddress: '',
-    });
+    setItem(barangDefaultValue);
   };
 
   const ColForm = ({ children, md = 6, label }: {
@@ -60,55 +70,99 @@ const InventoryForm = ({ onSave, editingItem }: { onSave: any, editingItem: any 
   return (
     <Form onSubmit={handleSubmit}>
       <Row>
-        <ColForm label='Nama Perangkat'>
+        <ColForm md={0} label='Nama Perangkat'>
           <Form.Control
             type="text"
             name="nama"
-            value={item.name}
+            value={item.nama}
             onChange={handleChange}
           />
         </ColForm>
-        <ColForm label='Jenis Perangkat'>
-          <Form.Select name="type" value={item.type} onChange={handleChange}>
-            <option value="goods">Goods</option>
-            <option value="it_device">IT Network Device</option>
-          </Form.Select>
-        </ColForm>
       </Row>
       <Row>
+        <ColForm label='Jenis Perangkat'>
+          <Form.Select name="type" value={item.jenis} onChange={handleChange}>
+            <option value="Goods">Goods</option>
+            <option value="IT Device">IT Network Device</option>
+          </Form.Select>
+        </ColForm>
         <ColForm label='Jumlah'>
           <Form.Control
             type="number"
             name="qty"
-            value={item.quantity}
+            value={item.qty}
             onChange={handleChange}
             min={1}
           />
+        </ColForm>
+      </Row>
+      <Row>
+                <ColForm label='Tahun Pengadaan'>
+          <Form.Control
+            type="date"
+            name="tahun_pengadaan"
+            value={item.tahunPengadaan}
+            onChange={handleChange}
+          />
+        </ColForm>
+        <ColForm label='Kondisi'>
+          <Form.Select name="kondisi" value={item.kondisi} onChange={handleChange}>
+            <option value="Baru">Baru</option>
+            <option value="Bagus">Bagus</option>
+            <option value="Kurang Baik">Kurang Baik</option>
+            <option value="Rusak">Rusak</option>
+          </Form.Select>
+        </ColForm>
+      </Row>
+      <Row>
+        <ColForm label='Status Operasional'>
+          <Form.Select name="status_op" value={item.kondisi} onChange={handleChange}>
+            <option value="1">Ya</option>
+            <option value="0">Tidak</option>
+          </Form.Select>
         </ColForm>
         <ColForm label='Lokasi'>
           <Form.Control
             type="text"
             name="lokasi"
-            value={item.location}
+            value={item.lokasi}
             onChange={handleChange}
           />
         </ColForm>
       </Row>
-      {item.type === 'it_device' && (
+      <Row>
+        <ColForm label='IP Address'>
+          <Form.Control
+            type="text"
+            name="ip"
+            value={item.ip}
+            onChange={handleChange}
+          />
+        </ColForm>
+        <ColForm label='Mac Address'>
+          <Form.Control
+            type="text"
+            name="mac"
+            value={item.mac}
+            onChange={handleChange}
+          />
+        </ColForm>
+      </Row>
+      {item.jenis === 'IT Device' && (
         <Row>
           <ColForm label='IP Address'>
             <Form.Control
               type="text"
-              name="ipAddress"
-              value={item.ipAddress}
+              name="ip"
+              value={item.ip}
               onChange={handleChange}
             />
           </ColForm>
           <ColForm label='IP Address'>
             <Form.Control
               type="text"
-              name="macAddress"
-              value={item.macAddress}
+              name="mac"
+              value={item.mac}
               onChange={handleChange}
             />
           </ColForm>
@@ -118,8 +172,8 @@ const InventoryForm = ({ onSave, editingItem }: { onSave: any, editingItem: any 
         <Form.Label className='fw-bold'>Keterangan</Form.Label>
         <Form.Control
           as="textarea"
-          name="description"
-          value={item.description}
+          name="ket"
+          value={item.ket}
           onChange={handleChange}
         />
       </Form.Group>
